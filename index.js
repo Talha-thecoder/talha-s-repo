0c5fbe1e39d9fbd2bec8ae6ff392fa7d58a50b04
Talha-thecoder <talha.codeing@gmail.com>
2024-07-29 10:05:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
-document.getElementById("myh1").textContent = "hello";
+let number = 34;
+number = 30;
+//number = number - 86;
+//number = number * 86;
+//number = number + 86;
+//number = number / 86;
+//number = number ** 86;
+number = number % 86;
+console.log(number);
+
+let static = 60;
+static = 29;
+//static = static - 86;
+//static = static * 86;
+//static = static + 86;
+//static = static / 86;
+//static = static ** 86;
+static = static % 86;
+console.log(static);
+
+let is = 60;
+is = 29;
+//is = is - 86;
+//is = is * 86;
+//is = is + 86;
+//is = is / 86;
+//is = is ** 86;
+isc = is % 86;
+console.log(is);
+
+let akula = 60;
+akula = 29;
+//akula = akula - 86;
+//akula = akula * 86;
+//akula = akula + 86;
+//akula = akula / 86;
+//akula = akula ** 86;
+akula = akula % 86;
+console.log(akula);
+
+let mei = 60;
+mei = 74;
+//mei = mei - 86;
+//mei = mei * 86;
+//mei = mei + 86;
+//mei = mei / 86;
+//mei = mei ** 86;
+mei = mei % 86;
+console.log(mei);
 
